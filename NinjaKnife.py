3eb4236372201891d743df613682713baf53f695
suntosh70 <santosh@santoshahuja.com>
2018-12-24 23:24:15 -0600

    Made http calls functional

--- NinjaKnife.py
+++ NinjaKnife.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 import globals
 import httplib
-import urllib
-
+import httplib
+import json
+import base64
+import ssl
 
 class NinjaKnife():
 	Get     =	"GET"
@@ -9,9 +12,20 @@ class NinjaKnife():
 
 	def __init__( self, method, apiurl):
 		self.method = method
-		self.apiurl = globals.SERVER_URL+apiurl
+		self.apiurl = apiurl
 
 	def fetch(self, params = ""):
 		self.params = params
-		print("hello world"+ params);
-
+		conn = httplib.HTTPSConnection(globals.SERVER_URL) 
+		conn._context.check_hostname = False 
+		conn._context.verify_mode = ssl.CERT_NONE
+		headers = {}
+		headers["Authorization"] = "Basic %s" % base64.standard_b64encode(globals.PASS_PHRASE)
+		req = conn.request(self.method, self.apiurl, headers=headers)
+		res = conn.getresponse()
+		if res.status == 200:
+			parsed = json.loads( res.read())
+			print json.dumps(parsed, indent=4, sort_keys=True)
+		else:
+			print res.status, "Error returned to in Response to the Request sent"
+		
